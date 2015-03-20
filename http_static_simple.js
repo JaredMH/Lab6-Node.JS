d26d87f57b3e1d148d45eb1922f588ed36d7264e
JaredMH <ec2-user@ip-172-31-39-134.us-west-2.compute.internal>
2015-03-20 05:45:34 +0000

    first commit

--- http_static_simple.js
+++ http_static_simple.js
@@ -9,7 +9,35 @@ http.createServer(function (req, res) {
   console.log("URL path " + urlObj.pathname);
   console.log("URL search " + urlObj.search);
   console.log("URL query " + urlObj.query["q"]);
-  if(urlObj.pathname.indexOf("getcity") != -1) {
+  
+  if(urlObj.pathname === "/comment") {
+    console.log("comment route");
+    if(req.method ==="POST") {
+      console.log("POST comment route");
+      //First read the form data
+      var jsonData = "";
+      req.on('data', function (chunk) {
+        jsonData += chunk;
+      });
+      req.on('end', function () {
+        var reqObj = JSON.parse(jsonData);
+        console.log(reqObj);
+        console.log("Name: "+reqObj.Name);
+        console.log("Comment: "+reqObj.Comment);
+     // ({});
+      // Now put it into the database
+      var MongoClient = require('mongodb').MongoClient;
+      MongoClient.connect("mongodb://localhost/weather", function(err, db) {
+        if(err) throw err;
+        db.collection('comments').insert(reqObj,function(err, records) {
+          console.log("Record added as ");//+records[0]._id);
+        });
+      });
+      });
+    res.writeHead(200);
+    res.end();
+    }
+  } else if(urlObj.pathname.indexOf("getcity") != -1) {
     console.log("in Getcity");
     var myRe = new RegExp("^"+urlObj.query["q"]);
     var jsonresult = [];
